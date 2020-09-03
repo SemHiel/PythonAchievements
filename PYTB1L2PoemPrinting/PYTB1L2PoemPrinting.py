591cd92e033db82d8c7c9d9d0a6f708400d89015
Sequos <66570022+SemHiel@users.noreply.github.com>
2020-09-03 14:40:22 +0200

    Gedicht

---
+++ PYTB1L2PoemPrinting/PYTB1L2PoemPrinting.py
@@ -0,0 +1,6 @@
+vers1 = "Hoi ik ben Sem,"
+vers2 = "\nEn ik ben aan het studeren,"
+vers3 = "\nMaar dat veranderd het feit niet,"
+vers4 = "\nDat ik niet kan dichten."
+
+print(vers1, vers2, vers3, vers4)
