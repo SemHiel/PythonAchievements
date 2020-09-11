06bf4ea3b55d176499f7135cdc31d261640ff447
Sequos <66570022+SemHiel@users.noreply.github.com>
2020-09-11 15:14:21 +0200

    operations added

---
+++ PYTB1L2MyFirstOperations/operations.py
@@ -0,0 +1,22 @@
+a = 10
+b = 5
+c = 2
+d = 3
+e = 5
+f = 2
+g = 5
+
+som1 = a + c
+print("Plus: ", som1)
+som2 = 9
+print("Toekennen: ", som2)
+som3 = a - g
+print("Aftrekken: ", som3)
+som4 = a / f
+print("Delen: ", som4)
+som4 += som1
+print("Optellen bij waarde: ", som4)
+som2 /= som3
+print("Delen door waarde:", som2)
+test = g < e
+print("Is variable E groter dan variable G?", test)
